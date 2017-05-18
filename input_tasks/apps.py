a6a0cef57834771cc813517c55619fda6be2f7d2
ezhov <kirill.ezhov@saprun.com>
2017-05-18 17:52:07 +0500

    Initial commit

---
+++ input_tasks/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InputTasksConfig(AppConfig):
+    name = 'input_tasks'
---
+++ input_tasks/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Task_type(models.Model):
+	ident = models.CharField(max_length=20)
+	description = models.CharField(max_length=200)
+
+class Urgency(models.Model):
+	ident = models.CharField(max_length=20)
+	description = models.CharField(max_length=200)
+
+class Type(models.Model):
+	ident = models.CharField(max_length=20)
+	description = models.CharField(max_length=200)
+
+class Task(models.Model):
+	task_type = models.ForeignKey(Task_type)
+	ident = models.CharField(max_length=40)
+	urgency = models.ForeignKey(Urgency)
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=2000)
+	special = models.CharField(max_length=300)
+	type = models.ForeignKey(Type)
+	due_date=models.DateField()
+	checklist=models.CharField(max_length=200)
+	labels=models.CharField(max_length=200)
+	sub_task=models.CharField(max_length=40)
+	atribute=models.CharField(max_length=10)
+	difficult=models.CharField(max_length=10)
+
